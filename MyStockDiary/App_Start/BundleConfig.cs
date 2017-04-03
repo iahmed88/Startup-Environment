efe82a4b8d1726e6869cc96cc740ae544895afeb
iahmed88 <iftikharahmed88@gmail.com>
2017-04-03 23:08:15 +0500

    Basic changes

---
+++ MyStockDiary/App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MyStockDiary.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular/angular.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/adminlte").Include(
+                "~/Scripts/adminlte/app.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/adminlte/AdminLTE.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/adminlte/skins/skin-blue.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/font-awesome/font-awesome.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/ionicons/ionicons.css"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+        }
+    }
+}
